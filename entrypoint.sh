dcef3c825ca3a0de4c940630d4a52bf90e15e1ff
Nat Weerawan <nat@cmmc.io>
2020-08-14 15:55:33 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ VERSION=$1
 PORT=$2
 PORT_WS=$3
 
-ls /tmp/sx
+ls /tmp/
 
-docker run -v /tmp/sx:/tmp/sx --rm --detach eclipse-mosquitto:$VERSION
+ docker run -ti -v /tmp/sx:/tmp/sx taisun/tmux /usr/bin/tmux -S /tmp/sx send-keys -t 0.0 'bash test.sh' C-m  
