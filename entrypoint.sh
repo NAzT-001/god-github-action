861e816585594d313e82fac9af365b02c5572e41
Nat Weerawan <nat@cmmc.io>
2020-08-14 11:38:22 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ VERSION=$1
 PORT=$2
 PORT_WS=$3
 
-docker run --name mosquitto --publish $PORT:$PORT --publish $PORT_WS:$PORT_WS --detach eclipse-mosquitto:$VERSION
+docker run -v /tmp/s:/tmp/s --name mosquitto --publish $PORT:$PORT --publish $PORT_WS:$PORT_WS --detach eclipse-mosquitto:$VERSION
