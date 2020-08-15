4eef1da695f5fac6c68c589c073a08d1e12b24b9
Nat Weerawan <nat@cmmc.io>
2020-08-15 09:06:51 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ PORT_WS=$3
 
 ls /tmp/
 
- docker run -ti -v /tmp/sx:/tmp/sx taisun/tmux /usr/bin/tmux -S /tmp/sx send-keys -t 0.0 'bash test.sh' C-m  
+ docker run -v /tmp/sx:/tmp/sx taisun/tmux /usr/bin/tmux -S /tmp/sx send-keys -t 0.0 'bash test.sh' C-m  
